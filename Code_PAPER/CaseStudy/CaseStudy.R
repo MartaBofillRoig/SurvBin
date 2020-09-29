4a88a4c953e4c60dd9e1338a1eaf883cc5975fd2
MBR <marta.bofill.roig@upc.edu>
2020-09-29 12:51:08 +0200

    update plots example

--- Code_PAPER/CaseStudy/CaseStudy.R
+++ Code_PAPER/CaseStudy/CaseStudy.R
@@ -1,6 +1,8 @@
 
-setwd("C:/Users/Marta.Bofill/Desktop/Code_PAPER/CaseStudy")
-load("C:/Users/Marta.Bofill/Desktop/Code_PAPER/CaseStudy/DigitizeIt/Dataset_Survival.RData")
+setwd("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/CaseStudy")
+load("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/CaseStudy/DigitizeIt/Dataset_Survival.RData")
+# setwd("C:/Users/mbofi/Desktop/Code_PAPER/CaseStudy")
+# load("C:/Users/mbofi/Desktop/Code_PAPER/CaseStudy/DigitizeIt/Dataset_Survival.RData")
 
 set.seed(2020)
 
@@ -25,12 +27,12 @@ head(data)
 ######################################
 
 sum(subset(data, data$treat==1)$binary)/n1
-sum(subset(data, data$treat==0)$binary)/n0 
+sum(subset(data, data$treat==0)$binary)/n0
 
 ######################################
 # PLOTS KAPLAN-MEIER ESTIMATORS
-######################################  
-library(rms) 
+######################################
+library(rms)
 windows()
 fit.rms <- npsurv(survival::Surv(time, status) ~ arm, data=recon_IPD)
 survplot(fit  = fit.rms,
@@ -41,14 +43,14 @@ survplot(fit  = fit.rms,
          label.curves = list(keys = "lines"),  # legend instead of direct label
          levels.only  = FALSE,                    # show only levels, no label
          abbrev.label = FALSE,                    # if label used, abbreviate
-         ## fun = function(x) {1 - x},            # Cumulative probability plot         
+         ## fun = function(x) {1 - x},            # Cumulative probability plot
          loglog   = FALSE,                        # log(-log Survival) plot
          logt     = FALSE,                        # log time
          time.inc = 1,                          # time increment
          dots     = TRUE,                         # dot grid
          n.risk   = TRUE,                          # show number at risk
          cex.n.risk=0.7,
-         col=c("coral1","chartreuse4"),lwd=2,lty=1,legend.pos = "topright" 
+         col=c("coral1","chartreuse4"),lwd=2,lty=1,legend.pos = "topright"
 )
 
 ######################################
@@ -56,14 +58,14 @@ survplot(fit  = fit.rms,
 
 
 # Functions for the binary and survival setting; for the covariance computation; and for simulating the binary and time-to-event data
-source('C:/Users/Marta.Bofill/Desktop/Code_PAPER/Functions/binary-functions.R')
-source('C:/Users/Marta.Bofill/Desktop/Code_PAPER/Functions/survival-functions.R') 
-source('C:/Users/Marta.Bofill/Desktop/Code_PAPER/Functions/cov-functions.R') 
-source('C:/Users/Marta.Bofill/Desktop/Code_PAPER/Functions/lstats-functions.R') 
+source('C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Functions/binary-functions.R')
+source('C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Functions/survival-functions.R')
+source('C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Functions/cov-functions.R')
+source('C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Functions/lstats-functions.R')
 
 require(zoo)
 require(survival)
-require(muhaz) 
+require(muhaz)
 
 B <- bintest(binary=data$binary, treat=data$treat, var_est = "Pooled")
 B[1]
@@ -80,4 +82,36 @@ z_sb[1]
 # Unpooled version
 # z_sb = lstats(time=data$time, status=data$status, binary=data$binary, treat=data$treat, tau0=0, tau=4, taub=0.5, rho=0, gam=1, eta=1, wb=0.25, ws=0.75, var_est = "Unpooled")
 # z_sb[1]
+
+install.packages("survRM2")
+library(survRM2)
+
+rmst2(time=data$time, status=data$status, arm=data$treat, tau=4)
+
+
+######################################
+# PLOTS HR Over time
+######################################
+windows()
+fit.rms <- npsurv(Surv(time=time,event=status)~treat, data)
+hzh <- hazard.ratio.plot(data$treat,Surv(time=data$time,event=data$status),  e=20,  xlim=c(0,30), legendloc='ll',antilog=T, xlab = "Time") #, ylim=c(0,1.5))
+
+se <- hzh$se[1,]
+lhr <- hzh$log.hazard.ratio[1,]
+t <- hzh$time
+hz <- exp(lhr)
+LI <- exp(lhr - 2*se)
+LS <- exp(lhr + 2*se)
+data2 <- data.frame(t,hz,LI,LS)
+
+library(ggplot2)
+# ggplot(data2,aes(x=t,y=hz)) + geom_line()
+ggplot(data2,aes(x=t,y=hz)) + geom_smooth(colour="#000099") + xlab('Time in years') + ylab("Hazard Ratio")  +theme(axis.text=element_text(size=12), axis.title=element_text(size=12,face="bold")) #+ ylim(0, 2)
+
+# library("survival")
+res.cox <- coxph(Surv(time=time,event=status)~treat, data)
+res.cox
+test.ph <- cox.zph(res.cox)
+test.ph
+
 
