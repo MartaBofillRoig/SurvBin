05e67992cca7d7f41dd6910172d41e33a404c9be
MBR <marta.bofill.roig@upc.edu>
2020-10-13 14:04:39 +0200

    nPH and eta

--- Code_PAPER/Extension_Simulation/Simulation_PAPER_PH.R
+++ Code_PAPER/Extension_Simulation/Simulation_PAPER_PH.R
@@ -464,7 +464,7 @@ theta=c(0.001,2,3)
 n=c(500)
 
 # test
-eta=c(0,1)
+eta=c(1)
 rho=c(0,1)
 gamma=c(0,1)
 omegab=c(0.5)
@@ -577,6 +577,137 @@ cat(t1, "\n", file="LOG_Results_H0.txt", append=TRUE)
 
 rm(i)
 save.image("C:/Users/Marta/Nextcloud/Gitkraken/SurvBin/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0.RData")
+# save.image("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0.RData")
+
+#####################################################################################
+# Scenarios H0 TRUE
+#####################################################################################
+
+a=c(0.5,1,2)
+b=c(1)
+tau=c(1)
+taub= c(0.5,1)
+r=c(3) #unif
+p0=c(0.1, 0.3)
+d=c(0)
+HR=c(1)
+theta=c(0.001,2,3)
+n=c(500)
+
+# test
+eta=c(0)
+rho=c(0,1)
+gamma=c(0,1)
+omegab=c(0.5)
+# omegab=c(0.25,0.5,0.75)
+
+data = tidyr::expand_grid(a,b,HR,tau,taub,r,p0,d,theta,n,eta,rho,gamma,omegab)
+
+data$p1=data$d+data$p0
+data$omegas=1-data$omegab
+
+# data = subset(data,data$eta+data$rho+data$gamma>0) #eta=1
+
+
+rm(a,b,taub,tau,r,p0,d,HR,omegab,theta,n,eta,rho,gamma)
+# save.image("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/database_Scenarios_H0_True.RData")
+# save.image("C:/Users/Marta/Nextcloud/Gitkraken/SurvBin/Code_PAPER/Extension_Simulation/results/database_Scenarios_H0_True.RData")
+
+
+# nsim: number of simulations
+nsim=1000
+
+#####################################################################################
+# simulation seed
+set.seed(1988)
+
+t0=Sys.time()
+data$Test_Alpha_pluginU=0
+data$Test_Alpha_pluginP=0
+data$Test_Alpha_Boots=0
+data$Test_Alpha_Bonf=0
+data$Test_Alpha_S=0
+data$Test_Alpha_B=0
+
+# for(i in 1:3){ # just for testing
+for(i in 1:dim(data)[1]){
+  
+  data$Test_Alpha_pluginU[i] <- sum(replicate(nsim,fCS.TEST_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                               rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                               ass.par=data$theta[i],
+                                                               n0=data$n[i]/2, n1=data$n[i]/2,
+                                                               censoring="Unif",
+                                                               tau=data$tau[i],
+                                                               taub=data$taub[i],
+                                                               rho=data$rho[i], gam=data$gamma[i], eta=data$eta[i],
+                                                               wb=data$omegab[i], ws=data$omegas[i],
+                                                               var_est='Unpooled')) > z.alpha)/nsim
+  
+  data$Test_Alpha_pluginP[i] <- sum(replicate(nsim,fCS.TEST_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                               rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                               ass.par=data$theta[i],
+                                                               n0=data$n[i]/2, n1=data$n[i]/2,
+                                                               censoring="Unif",
+                                                               tau=data$tau[i],
+                                                               taub=data$taub[i],
+                                                               rho=data$rho[i], gam=data$gamma[i], eta=data$eta[i],
+                                                               wb=data$omegab[i], ws=data$omegas[i],
+                                                               var_est='Pooled')) > z.alpha)/nsim
+  
+  data$Test_Alpha_Boots[i] <- sum(replicate(nsim,fCS.TEST_boots_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                                   rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                                   ass.par=data$theta[i],
+                                                                   n0=data$n[i]/2, n1=data$n[i]/2,
+                                                                   censoring="Unif",
+                                                                   tau=data$tau[i],
+                                                                   taub=data$taub[i],
+                                                                   rho=data$rho[i], gam=data$gamma[i], eta=data$eta[i],
+                                                                   wb=data$omegab[i],
+                                                                   ws=data$omegas[i],
+                                                                   Boot=50)) > z.alpha)/nsim
+  
+  data$Test_Alpha_Bonf[i] <- sum(replicate(nsim,(sum(fCS.TEST_Bonf_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                                      rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                                      ass.par=data$theta[i],
+                                                                      n0=data$n[i]/2, n1=data$n[i]/2,
+                                                                      censoring="Unif",
+                                                                      tau=data$tau[i],
+                                                                      taub=data$taub[i],
+                                                                      rho=data$rho[i], gam=data$gamma[i],
+                                                                      eta=data$eta[i])>z.alphac)>=1)))/nsim
+  
+  data$Test_Alpha_S[i] <- sum(replicate(nsim,fCS.TEST_Bonf_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                              rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                              ass.par=data$theta[i],
+                                                              n0=data$n[i]/2, n1=data$n[i]/2,
+                                                              censoring="Unif",
+                                                              tau=data$tau[i],
+                                                              taub=data$taub[i],
+                                                              rho=data$rho[i], gam=data$gamma[i],
+                                                              eta=data$eta[i])[2]) > z.alpha)/nsim
+  
+  data$Test_Alpha_B[i] <- sum(replicate(nsim,fCS.TEST_Bonf_H1(a.shape=data$a[i], b.scale=data$b[i], HR=1,
+                                                              rate.param=data$r[i], p0=data$p0[i], p1=data$p0[i],
+                                                              ass.par=data$theta[i],
+                                                              n0=data$n[i]/2, n1=data$n[i]/2,
+                                                              censoring="Unif",
+                                                              tau=data$tau[i],
+                                                              taub=data$taub[i],
+                                                              rho=data$rho[i], gam=data$gamma[i],
+                                                              eta=data$eta[i])[1]) > z.alpha)/nsim
+  
+  t1=Sys.time()-t0
+  cat(i, "\t", t1, "\n", file="LOG_Results_H0_eta.txt", append=TRUE)
+  save.image("C:/Users/Marta/Nextcloud/Gitkraken/SurvBin/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0_eta.RData")
+  # save.image("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0.RData")
+}
+
+t1=Sys.time()-t0
+cat(t1, "\n", file="LOG_Results_H0_eta.txt", append=TRUE)
+(t1)
+
+rm(i)
+save.image("C:/Users/Marta/Nextcloud/Gitkraken/SurvBin/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0_eta.RData")
 # save.image("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0.RData")
 
 #####################################################################################
