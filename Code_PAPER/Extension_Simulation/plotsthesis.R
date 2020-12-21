6e5dc27a0916d71f1ea37542030b97aa940303f6
MBR <marta.bofill.roig@upc.edu>
2020-12-21 10:57:20 +0100

    plot thesis

--- Code_PAPER/Extension_Simulation/plotsthesis.R
+++ Code_PAPER/Extension_Simulation/plotsthesis.R
@@ -284,264 +284,53 @@ windows()
 plot_case1
 
 ########
+################################################################
+# Plot per case
+################################################################
+data_H1  = subset(data_H1,data_H1$omegab==0.5)
+colors <- c("0.25" = "#FD6467", "0.75" = "#018F00", "Multiple test" = "#0001CE", "Individual tests" = "#333C45")
 
-# Create plot with legend
-ggp1_legend <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot() + scale_color_manual(values = colors)+theme(legend.position = "bottom")  + labs(color='Weight (wb)')  #+ labs(color='Weight (expression(omega)b)')
+# case1
+power_data <- data.frame(Power=c(data_H1[data_H1$cases==1,]$Test_Power_pluginU,
+                                 # data_H1[data_H1$cases==1,]$Test_Power_pluginP,
+                                 data_H1[data_H1$cases==1,]$Test_Power_Boots,
+                                 data_H1[data_H1$cases==1,]$Test_Power_Bonf
+                                 # ,
+                                 # data_H1[data_H1$cases==1,]$Test_Power_B,
+                                 # data_H1[data_H1$cases==1,]$Test_Power_S
+),
+Test=c(rep("Plug-in",length(data_H1[data_H1$cases==1,]$Test_Power_pluginU)),
+       # rep("Pooled",length(data_H1[data_H1$cases==1,]$Test_Power_pluginP)),
+       rep("Bootstrap",length(data_H1[data_H1$cases==1,]$Test_Power_Boots)),
+       rep("Bonferroni",length(data_H1[data_H1$cases==1,]$Test_Power_Bonf))
+       # ,
+       # rep("Binary test",length(data_H1[data_H1$cases==1,]$Test_Power_B)),
+       # rep("Survival test",length(data_H1[data_H1$cases==1,]$Test_Power_S))
+),
+omegab=c(rep("Multiple test",length(data_H1[data_H1$cases==1,]$omegab)),
+         rep("Multiple test",length(data_H1[data_H1$cases==1,]$omegab)),
+         rep("Multiple test",length(data_H1[data_H1$cases==1,]$omegab))
+         # ,
+         # data_H1[data_H1$cases==1,]$omegab,
+         #  # data_H1[data_H1$cases==1,]$omegab,
+         #  data_H1[data_H1$cases==1,]$omegab,
+         #  rep(0.5,length(data_H1[data_H1$cases==1,]$omegab)),
+         # rep("Individual tests",length(data_H1[data_H1$cases==1,]$omegab)),
+         # rep("Individual tests",length(data_H1[data_H1$cases==1,]$omegab))
+)
+)
 
-# Create user-defined function, which extracts legends from ggplots
-extract_legend <- function(my_ggp) {
-  step1 <- ggplot_gtable(ggplot_build(my_ggp))
-  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
-  step3 <- step1$grobs[[step2]]
-  return(step3)
-}
+power_data$Test <- factor(power_data$Test,
+                          levels = c('Bootstrap', 'Plug-in',
+                                     # 'Pooled',
+                                     'Bonferroni'
+                                     # , 'Binary test', 'Survival test'
+                          ),
+                          ordered = TRUE)
 
-# Apply user-defined function to extract legend
-shared_legend <- extract_legend(ggp1_legend)
+plot_case1 <- ggplot(power_data, aes(x=Test, y=Power, color=as.factor(omegab))) + geom_boxplot() + scale_color_manual(values = colors) +theme(legend.position = "bottom")  + labs(color='Univariate or multiple tests')
+
+windows()
+plot_case1
 
 ########
-
-# case2
-power_data <- data.frame(power=c(data_H1[data_H1$cases==2,]$Test_Power_pluginU,
-                                 data_H1[data_H1$cases==2,]$Test_Power_pluginP,
-                                 data_H1[data_H1$cases==2,]$Test_Power_Boots,
-                                 data_H1[data_H1$cases==2,]$Test_Power_Bonf,
-                                 data_H1[data_H1$cases==2,]$Test_Power_B,
-                                 data_H1[data_H1$cases==2,]$Test_Power_S
-),
-Test=c(rep("Unpooled",length(data_H1[data_H1$cases==2,]$Test_Power_pluginU)),
-       rep("Pooled",length(data_H1[data_H1$cases==2,]$Test_Power_pluginP)),
-       rep("Bootstrap",length(data_H1[data_H1$cases==2,]$Test_Power_Boots)),
-       rep("Bonferroni",length(data_H1[data_H1$cases==2,]$Test_Power_Bonf)),
-       rep("BE",length(data_H1[data_H1$cases==2,]$Test_Power_B)),
-       rep("SE",length(data_H1[data_H1$cases==2,]$Test_Power_S))
-),
-omegab=c(data_H1[data_H1$cases==2,]$omegab,
-         data_H1[data_H1$cases==2,]$omegab,
-         data_H1[data_H1$cases==2,]$omegab,
-         rep(0.5,length(data_H1[data_H1$cases==2,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==2,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==2,]$omegab))
-)
-)
-
-power_data$Test <- factor(power_data$Test,
-                          levels = c('Bootstrap', 'Unpooled', 'Pooled', 'Bonferroni', 'BE','SE'),
-                          ordered = TRUE)
-
-plot_case2 <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot() + scale_color_manual(values = colors) + ggtitle("Case 2") +  theme(legend.position = "none")
-
-# case3
-power_data <- data.frame(power=c(data_H1[data_H1$cases==3,]$Test_Power_pluginU,
-                                 data_H1[data_H1$cases==3,]$Test_Power_pluginP,
-                                 data_H1[data_H1$cases==3,]$Test_Power_Boots,
-                                 data_H1[data_H1$cases==3,]$Test_Power_Bonf,
-                                 data_H1[data_H1$cases==3,]$Test_Power_B,
-                                 data_H1[data_H1$cases==3,]$Test_Power_S
-),
-Test=c(rep("Unpooled",length(data_H1[data_H1$cases==3,]$Test_Power_pluginU)),
-       rep("Pooled",length(data_H1[data_H1$cases==3,]$Test_Power_pluginP)),
-       rep("Bootstrap",length(data_H1[data_H1$cases==3,]$Test_Power_Boots)),
-       rep("Bonferroni",length(data_H1[data_H1$cases==3,]$Test_Power_Bonf)),
-       rep("BE",length(data_H1[data_H1$cases==3,]$Test_Power_B)),
-       rep("SE",length(data_H1[data_H1$cases==3,]$Test_Power_S))
-),
-omegab=c(data_H1[data_H1$cases==3,]$omegab,
-         data_H1[data_H1$cases==3,]$omegab,
-         data_H1[data_H1$cases==3,]$omegab,
-         rep(0.5,length(data_H1[data_H1$cases==3,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==3,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==3,]$omegab))
-)
-)
-
-power_data$Test <- factor(power_data$Test,
-                          levels = c('Bootstrap', 'Unpooled', 'Pooled', 'Bonferroni', 'BE','SE'),
-                          ordered = TRUE)
-
-plot_case3 <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot() + scale_color_manual(values = colors) + ggtitle("Case 3") +  theme(legend.position = "none")
-
-
-# case4
-power_data <- data.frame(power=c(data_H1[data_H1$cases==4,]$Test_Power_pluginU,
-                                 data_H1[data_H1$cases==4,]$Test_Power_pluginP,
-                                 data_H1[data_H1$cases==4,]$Test_Power_Boots,
-                                 data_H1[data_H1$cases==4,]$Test_Power_Bonf,
-                                 data_H1[data_H1$cases==4,]$Test_Power_B,
-                                 data_H1[data_H1$cases==4,]$Test_Power_S
-),
-Test=c(rep("Unpooled",length(data_H1[data_H1$cases==4,]$Test_Power_pluginU)),
-       rep("Pooled",length(data_H1[data_H1$cases==4,]$Test_Power_pluginP)),
-       rep("Bootstrap",length(data_H1[data_H1$cases==4,]$Test_Power_Boots)),
-       rep("Bonferroni",length(data_H1[data_H1$cases==4,]$Test_Power_Bonf)),
-       rep("BE",length(data_H1[data_H1$cases==4,]$Test_Power_B)),
-       rep("SE",length(data_H1[data_H1$cases==4,]$Test_Power_S))
-),
-omegab=c(data_H1[data_H1$cases==4,]$omegab,
-         data_H1[data_H1$cases==4,]$omegab,
-         data_H1[data_H1$cases==4,]$omegab,
-         rep(0.5,length(data_H1[data_H1$cases==4,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==4,]$omegab)),
-         rep("Individual tests",length(data_H1[data_H1$cases==4,]$omegab))
-)
-)
-
-power_data$Test <- factor(power_data$Test,
-                          levels = c('Bootstrap', 'Unpooled', 'Pooled', 'Bonferroni', 'BE','SE'),
-                          ordered = TRUE)
-
-plot_case4 <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot() + scale_color_manual(values = colors) + ggtitle("Case 4") +   theme(legend.position = "none")
-
-windows()
-# Draw plots with shared legend
-grid.arrange(arrangeGrob(plot_case1, plot_case2, plot_case3,plot_case4,  ncol = 2),
-             shared_legend, nrow = 2, heights = c(10, 1), top = "Empirical Powers")
-
-
-windows()
-grid.arrange(arrangeGrob(plot_case1, plot_case4,  ncol = 2),
-             shared_legend, nrow = 2, heights = c(10, 1), top = "Empirical Powers (Effect on both endpoints)")
-
-grid.arrange(arrangeGrob(plot_case2, plot_case3, ncol = 2),
-             shared_legend, nrow = 2, heights = c(10, 1), top = "Empirical Powers (Effect only on one endpoint)")
-
-
-################################################################
-################################################################
-
-################################################################
-# Unified version results
-# Under H0
-################################################################
-
-rm(list = ls())
-load("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_H0.RData")
-summary(data)
-
-# General summary powers
-summary(data[,17:22])
-
-# taub
-summary(data[data$taub==0.5,17:20])
-summary(data[data$taub==1,17:20])
-
-summary(data[data$taub==1 & data$p0==0.1,17:20])
-summary(data[data$taub==1 & data$p0==0.3,17:20])
-
-
-# Theta
-summary(data[data$theta==0.001,17:20])
-summary(data[data$theta==2,17:20])
-summary(data[data$theta==3,17:20])
-
-# Rho and Gamma
-summary(data[data$gamma==1 & data$rho==1,17:20])
-summary(data[data$gamma==0 & data$rho==1,17:20])
-summary(data[data$gamma==1 & data$rho==0,17:20])
-summary(data[data$gamma==0 & data$rho==0,17:20])
-
-# Gamma (late-effects)
-summary(data[data$gamma==1,17:20])
-summary(data[data$gamma==0,17:20])
-
-# Rho (censoring)
-summary(data[data$rho==1,17:20])
-summary(data[data$rho==0,17:20])
-
-# p0 (more/less effect binary)
-summary(data[data$p0==0.1,17:22])
-summary(data[data$p0==0.3,17:22])
-
-
-# a
-summary(data[data$a==0.5,17:20])
-summary(data[data$a==1,17:20])
-summary(data[data$a==2,17:20])
-
-
-################################################################
-# ADDITIONAL RESULTS
-################################################################
-
-load("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_add.RData")
-
-data_H1=data
-
-# add1
-
-colors <- c("0.25" = "#FD6467", "0.75" = "#018F00", "0.5" = "#0001CE", "Individual tests" = "#333C45")
-power_data <- data.frame(power=c(data_H1$Test_Power_pluginU,
-                                 data_H1$Test_Power_pluginP,
-                                 data_H1$Test_Power_Boots,
-                                 data_H1$Test_Power_Bonf,
-                                 data_H1$Test_Power_B,
-                                 data_H1$Test_Power_S
-),
-Test=c(rep("Unpooled",length(data_H1$Test_Power_pluginU)),
-       rep("Pooled",length(data_H1$Test_Power_pluginP)),
-       rep("Bootstrap",length(data_H1$Test_Power_Boots)),
-       rep("Bonferroni",length(data_H1$Test_Power_Bonf)),
-       rep("BE",length(data_H1$Test_Power_B)),
-       rep("SE",length(data_H1$Test_Power_S))
-),
-omegab=c(data_H1$omegab,
-         data_H1$omegab,
-         data_H1$omegab,
-         rep(0.5,length(data_H1$omegab)),
-         rep("Individual tests",length(data_H1$omegab)),
-         rep("Individual tests",length(data_H1$omegab))
-)
-)
-
-power_data$Test <- factor(power_data$Test,
-                          levels = c('Bootstrap', 'Unpooled', 'Pooled', 'Bonferroni', 'BE','SE'),
-                          ordered = TRUE)
-
-plot_add1 <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot()  + scale_color_manual(values = colors) + ggtitle("Small Effect Binary Endpoint") +theme(legend.position = "bottom")  + labs(color='Weight (wb)')
-# +   theme(legend.position = "none")
-
-windows()
-plot_add1
-
-
-################################################################
-
-load("C:/Users/mbofi/Dropbox/C5/Scripts/GitKraken/survivalbinary/Code_PAPER/Extension_Simulation/results/RESULTS_PAPER_add2.RData")
-
-data_H1=data
-
-# add2
-
-colors <- c("0.25" = "#FD6467", "0.75" = "#018F00", "0.5" = "#0001CE", "Individual tests" = "#333C45")
-power_data <- data.frame(power=c(data_H1$Test_Power_pluginU,
-                                 data_H1$Test_Power_pluginP,
-                                 data_H1$Test_Power_Boots,
-                                 data_H1$Test_Power_Bonf,
-                                 data_H1$Test_Power_B,
-                                 data_H1$Test_Power_S
-),
-Test=c(rep("Unpooled",length(data_H1$Test_Power_pluginU)),
-       rep("Pooled",length(data_H1$Test_Power_pluginP)),
-       rep("Bootstrap",length(data_H1$Test_Power_Boots)),
-       rep("Bonferroni",length(data_H1$Test_Power_Bonf)),
-       rep("BE",length(data_H1$Test_Power_B)),
-       rep("SE",length(data_H1$Test_Power_S))
-),
-omegab=c(data_H1$omegab,
-         data_H1$omegab,
-         data_H1$omegab,
-         rep(0.5,length(data_H1$omegab)),
-         rep("Individual tests",length(data_H1$omegab)),
-         rep("Individual tests",length(data_H1$omegab))
-)
-)
-
-power_data$Test <- factor(power_data$Test,
-                          levels = c('Bootstrap', 'Unpooled', 'Pooled', 'Bonferroni', 'BE','SE'),
-                          ordered = TRUE)
-
-plot_add2 <- ggplot(power_data, aes(x=Test, y=power, color=as.factor(omegab))) + geom_boxplot()  + scale_color_manual(values = colors) + ggtitle("Small Effect Survival Endpoint") +theme(legend.position = "bottom")  + labs(color='Weight (wb)')
-# +   theme(legend.position = "none")
-
-windows()
-plot_add2
